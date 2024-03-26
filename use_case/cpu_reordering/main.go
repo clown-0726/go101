ddfbd2c88bc0d8ad565b91da2d23c448dbb10f72
Lilu Cao <lilu.cao@orbitfin.ai>
2024-03-26 11:08:20 +0800

    Add cpu reorderding experiment.

---
+++ use_case/cpu_reordering/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func withCpuReordering() {
+	index := 0
+	for {
+		index += 1
+
+		var a, b int32 = 0, 0
+		var x, y int32 = 0, 0
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+
+			atomic.StoreInt32(&a, 1)
+			atomic.StoreInt32(&x, atomic.LoadInt32(&b))
+		}()
+
+		go func() {
+			defer wg.Done()
+
+			atomic.StoreInt32(&b, 1)
+			atomic.StoreInt32(&y, atomic.LoadInt32(&a))
+		}()
+		wg.Wait()
+
+		if x == 0 && y == 0 {
+			panic("CPU Reording occurs!")
+		} else {
+			fmt.Println("Now processing in loop", index)
+		}
+	}
+}
+
+func withoutCpuReordering() {
+	index := 0
+	for {
+		index += 1
+
+		var a, b int32 = 0, 0
+		var x, y int32 = 0, 0
+
+		var wg sync.WaitGroup
+		wg.Add(2)
+
+		go func() {
+			defer wg.Done()
+
+			a = 1
+			x = b
+		}()
+
+		go func() {
+			defer wg.Done()
+
+			b = 1
+			y = a
+		}()
+		wg.Wait()
+
+		if x == 0 && y == 0 {
+			panic("CPU Reording occurs!")
+		} else {
+			fmt.Println("Now processing in loop", index)
+		}
+	}
+}
+
+func main() {
+	// 在第 1738, 110002, 12987 次测试到了 CPU 指令重排序
+	withoutCpuReordering()
+
+	// 一直到 1370370 也没测试到 CPU 指令重排序
+	withCpuReordering()
+}
